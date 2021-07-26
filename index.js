7dfdcd2249d177327a84bd4e8099352e21db99ef
Paul Fell <swellpf@gmail.com>
2021-07-26 15:10:14 -0400

    Created largest subarray sum function

---
+++ index.js
@@ -0,0 +1,12 @@
+function largestSubarraySum(array) {
+let maxSoFar = array[0];
+let globalMax = array[0];
+for (let i=1; i < array.length; i++) {
+    globalMax = Math.max(globalMax + array[i], array[i]);
+    maxSoFar = Math.max(maxSoFar, globalMax);
+}
+maxSoFar > 0 ? maxSoFar : maxSoFar = 0
+return maxSoFar;
+}
+
+largestSubarraySum(array)
